f579346b2bcc93e0ad9e3b35ade0abd4d666c122
kiran paturi <kpaturi@smarttuition.com>
2016-08-08 14:39:25 +0530

    Added code for register and login

---
+++ app/app.js
@@ -0,0 +1,17 @@
+angular.module("myntra", ["main", "ui.router"]);
+
+
+//configure routing
+angular.module("myntra")
+    .config(function ($stateProvider) {
+
+        var loginObj = {
+            templateUrl: "app/templates/login.html"
+        };
+        var registerObj = {
+            templateUrl: "app/templates/register.html"
+        };
+
+        $stateProvider.state("login", loginObj);
+        $stateProvider.state("register", registerObj);
+    });
